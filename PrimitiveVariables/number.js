3ae310f8944d83f43346537c6f77c543caac334c
Aleksander Alencar Junior <aleksalencar7@gmail.com>
2021-02-19 22:49:09 -0300

    Numbers

---
+++ PrimitiveVariables/number.js
@@ -0,0 +1,13 @@
+// Typeof
+console.log(typeof 5)
+console.log(typeof 12.1)
+console.log(typeof -12313)
+
+// Aritimectic expressions
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 / 2)
+console.log(2 * 2)
+console.log( 2 + (2 * 4))
+console.log( 23 % 2)
+
