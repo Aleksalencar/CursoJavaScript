bf3844a1cc1241770cb69bcdf4f9d0f1ada83ba1
Aleksander Alencar Junior <aleksalencar7@gmail.com>
2021-03-13 20:37:23 -0300

    Arithmetic

--- PrimitiveVariables/number.js
+++ PrimitiveVariables/number.js
@@ -8,6 +8,15 @@ console.log(2 + 2)
 console.log(2 - 2)
 console.log(2 / 2)
 console.log(2 * 2)
-console.log( 2 + (2 * 4))
-console.log( 23 % 2)
+console.log(2 + (2 * 4))
+console.log(23 % 2)
+
+//Special numbers
+console.log(Infinity)
+console.log(-Infinity)
+console.log(NaN)
+
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(typeof NaN)
 
---
+++ PrimitiveVariables/strings.js
@@ -0,0 +1,8 @@
+console.log("Olá mundo")
+console.log('Aspas simples')
+console.log(`daww`)
+
+console.log("Linha 1\nLinha 2")
+var res = 2 + 2
+console.log(`A soma de 2 + 2 é ${res}`) // String em crase, sintax: ${code}
+console.log("Suco " + "de " + "uva")
