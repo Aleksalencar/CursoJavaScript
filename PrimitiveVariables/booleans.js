705c8d8978bf7ba4df024a264f9092a84dde7ca6
Aleksander Alencar Junior <aleksalencar7@gmail.com>
2021-03-14 02:28:05 -0300

    ;

--- PrimitiveVariables/booleans.js
+++ PrimitiveVariables/booleans.js
@@ -1,56 +1,56 @@
 // Booleans
 
-var flag = true
+var flag = true;
 if (flag === true){
-    console.log("flag é verdadeira")    
+    console.log("flag é verdadeira");    
 }else{
-    console.log("flag é falsa")    
+    console.log("flag é falsa"); 
 }
 
-flag = false
+flag = false;
 if (flag){
-    console.log("flag é verdadeira")    
+    console.log("flag é verdadeira");    
 }else{
-    console.log("flag é falsa")    
+    console.log("flag é falsa");    
 }
 
 
 // comparaçoes
-console.log("10 > 1 " + 10 > 1)
-console.log(10 < 1)
-console.log(10 >= 10)
-console.log(10 == 10)
-console.log(flag === false)
-console.log(10 != 10)
+console.log("10 > 1 " + 10 > 1);
+console.log(10 < 1);
+console.log(10 >= 10);
+console.log(10 == 10);
+console.log(flag === false);
+console.log(10 != 10);
 
 // Operadores logicos
-var num = 10
-flag = true
+var num = 10;
+flag = true;
 if(num == 10 && flag === true){ // and 
-    console.log("top")
+    console.log("top");
 
 }
 
 if(num != 10 || flag === true){ // or
-    console.log("top 2")
+    console.log("top 2");
 
 }
 
-flag = false
-if (!flag){ // nor
-    console.log("top 3")
+flag = false;
+if (!flag){ // not
+    console.log("top 3");
 }
 
 // Ternários
-console.log(true ? 1 : 2)
+console.log(true ? 1 : 2);
 
-console.log(false ? 'verdadeiro' : 'falso')
+console.log(false ? 'verdadeiro' : 'falso');
 
-flag = true
-console.log(flag ? 'verdadeiro' : 'falso')
+flag = true;
+console.log(flag ? 'verdadeiro' : 'falso');
 
-// Conversão automatico
-console.log(5 * null) // 0
-console.log("5" - 3) // 2
-console.log("5" + 1) // 51
-console.log("dois" * "três") // NaN
+// Conversão automatico (WTF)
+console.log(5 * null); // 0
+console.log("5" - 3); // 2
+console.log("5" + 1); // 51
+console.log("dois" * "três"); // NaN
